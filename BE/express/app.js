67b07f275756a372617fc862078a0fa11ad91cbc
nambin <mkkim044@naver.com>
2025-02-17 11:03:29 +0900

    5주차 01 get , post 메소드

--- BE/express/app.js
+++ BE/express/app.js
@@ -7,5 +7,15 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
-  res.json();
+  res.send('success get method');
+});
+
+//body로 받은 data 세팅
+app.use(express.json());
+
+//body로 받은 data 뿌려주기
+app.post('/test', (req, res) => {
+  const showData = req.body;
+
+  res.json(showData);
 });
--- BE/server-api/youtuber-demo.js
+++ BE/server-api/youtuber-demo.js
@@ -5,32 +5,36 @@ app.listen(3333, () => {
   console.log('server debuging...');
 });
 
+app.use(express.json()); // http 외 모듈인 `미들웨어` json 설정
+
 // 데이터 세팅
 let youtuber1 = {
   channelTitle: '십오야',
   sub: 7000000,
-  vidoNum: 1000,
+  videoNum: 1000,
 };
 
 let youtuber2 = {
   channelTitle: '침착맨',
   sub: 3000000,
-  vidoNum: 7000,
+  videoNum: 7000,
 };
 
 let youtuber3 = {
   channelTitle: 'Teo',
   sub: 2500000,
-  vidoNum: 1600,
+  videoNum: 1600,
 };
 
 let db = new Map();
-db.set(1, youtuber1);
-db.set(2, youtuber2);
-db.set(3, youtuber3);
+var id = 1;
+
+db.set(id, youtuber1);
+db.set(++id, youtuber2);
+db.set(++id, youtuber3);
 
 // REST API 설계
-app.get('/youtuber/:id', (req, res) => {
+app.get('/youtubers/:id', (req, res) => {
   let {id} = req.params;
   id = Number(id);
 
@@ -41,4 +45,31 @@ app.get('/youtuber/:id', (req, res) => {
   } else {
     res.json(youtuber);
   }
+});
+
+//전체 유튜버 조회 (GET/youtubers)
+// req : x
+// res :
+app.get('/youtubers', (req, res) => {
+  const showData = Array.from(db);
+  res.json(showData);
+});
+
+// post를 이용해서 유튜버 (data) 추가해보기!!
+// 1. API 설계 (URL , Method)
+// -req : params.id : Map에 저장된 key 값을 전달
+// -res : map에서 id로 객체를 조회해서 전달
+
+// 2. 유튜버 등록 => POST /youtuber
+// req : body (channelTitle, sub = 0, vidoNum = 0,)
+// res : channelTitle을 이용한 message 보내기
+
+app.post('/youtubers', (req, res) => {
+  let data = req.body;
+  console.log(data);
+
+  db.set(++id, data);
+  res.json({
+    message: `${db.get(id).channelTitle}님 유튜버 생활을 응원합니다`,
+  });
 });
