c33aa9c9154726994522abdbad88e3a3d3a0e75f
nambin <mkkim044@naver.com>
2025-03-13 10:52:12 +0900

    8주치 04 도서API 신간 + 페이징 추가

--- PageTurner/controller/book-controller.js
+++ PageTurner/controller/book-controller.js
@@ -1,42 +1,48 @@
 const conn = require('../db');
 const {StatusCodes} = require('http-status-codes');
 
-//도서 전체 조회 + category
+//도서 전체 조회 + category별 조회 + category별 신간 조회
 const all_books = (req, res) => {
-  let {category_id} = req.query;
+  let {category_id, new_book, limit, currentPage} = req.query;
 
-  // category
-  if (category_id) {
-    let sql = `SELECT * FROM books 
-    WHERE category_id = ?`;
-    conn.query(
-      sql,
-      id,
+  let offset = limit * (currentPage - 1);
 
-      (err, result) => {
-        if (err) {
-          return res.status(StatusCodes.BAD_REQUEST).end();
-        }
+  let sql = `SELECT * FROM books `;
+  let values = [];
 
-        if (result.length) {
-          return res.status(StatusCodes.OK).json(result);
-        } else {
-          return res.status(StatusCodes.NOT_FOUND).end();
-        }
-      },
-    );
+  if (category_id && new_book) {
+    sql += ` WHERE category_id=? AND pub_date 
+    BETWEEN DATE_SUB(NOW() , INTERVAL 1 MONTH)
+    AND NOW() `;
+    values = [Number(category_id)];
+  } else if (category_id) {
+    sql += `WHERE category_id=? `;
+    values = [category_id];
+  } else if (new_book) {
+    sql += ` WHERE pub_date 
+    BETWEEN  DATE_SUB(NOW() , INTERVAL 1 MONTH)
+    AND NOW() `;
   }
-  //all book
-  else {
-    let sql = `SELECT * FROM books`;
-    conn.query(sql, (err, result) => {
+  sql += `LIMIT ? OFFSET ?`;
+  values.push(Number(limit), offset);
+
+  conn.query(
+    sql,
+    values,
+
+    (err, result) => {
+      console.log(sql, values);
       if (err) {
         return res.status(StatusCodes.BAD_REQUEST).end();
       }
 
-      return res.status(StatusCodes.OK).json(result);
-    });
-  }
+      if (result.length) {
+        return res.status(StatusCodes.OK).json(result);
+      } else {
+        return res.status(StatusCodes.NOT_FOUND).end();
+      }
+    },
+  );
 };
 
 // book detail
