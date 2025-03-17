09cef1068365e69f9c465759e7801ea7d9edf34b
nambin <mkkim044@naver.com>
2025-03-17 11:07:43 +0900

    9주차 01 장바구니api 구현

---
+++ PageTurner/controller/cart-controller.js
@@ -0,0 +1,70 @@
+const conn = require('../db');
+const {StatusCodes} = require('http-status-codes');
+
+// 카트 추가
+const addCart = (req, res) => {
+  const {cart_book_id, quantity, cart_user_id} = req.body;
+
+  let sql = `INSERT INTO cartItems (cart_book_id, quantity, cart_user_id) VALUES (?, ?, ?);`;
+  let values = [cart_book_id, quantity, cart_user_id];
+  conn.query(
+    sql,
+    values,
+
+    (err, result) => {
+      if (err) {
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+      return res.status(StatusCodes.OK).json(result);
+    },
+  );
+};
+
+// 장바구니 조회 + 예상 장바구니 목록 조회
+// 장바구니 조회 + 예상 장바구니 목록 조회
+const getCart = (req, res) => {
+  const {cart_user_id, seleted} = req.body;
+
+  let sql = `
+      SELECT cartItems.id, cart_book_id, title, summary, quantity, price 
+      FROM cartItems LEFT JOIN books 
+      ON cartItems.cart_book_id = books.id
+      WHERE cart_user_id = ? 
+      AND  cartItems.id IN(?)`;
+
+  conn.query(
+    sql,
+    [cart_user_id, seleted],
+
+    (err, result) => {
+      if (err) {
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+
+      return res.status(StatusCodes.OK).json(result);
+    },
+  );
+};
+
+// 장바구니 삭제
+const removeCartItem = (req, res) => {
+  const {id} = req.params;
+
+  let sql = `
+  DELETE FROM cartItems WHERE id = ?`;
+
+  conn.query(
+    sql,
+    id,
+
+    (err, result) => {
+      if (err) {
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+
+      return res.status(StatusCodes.OK).json(result);
+    },
+  );
+};
+
+module.exports = {addCart, getCart, removeCartItem};
--- PageTurner/routes/cart.js
+++ PageTurner/routes/cart.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const {
+  addCart,
+  getCart,
+  removeCartItem,
+} = require('../controller/cart-controller');
 
 router.use(express.json());
 
 // add cart
-router.post('/', (req, res) => {
-  res.json('add cart');
-});
+router.post('/', addCart);
 
-// select cart
-router.get('/', (req, res) => {
-  res.json('add cart...');
-});
+// select cart + (주문 예상 목록)
+router.get('/', getCart);
 
 // del cart
-router.delete('/:id', (req, res) => {
-  res.json('Nooooo cart');
-});
-
-// cart list (주문 예상 목록)
-router.get('/', (req, res) => {
-  res.json('cart list');
-});
+router.delete('/:id', removeCartItem);
 
 module.exports = router;
